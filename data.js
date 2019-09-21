84d9afdb14556631b8f174e0ee0e4c4a4f47008b
Robbert Ruiter <ro-ru@live.nl>
2019-09-21 13:34:21 +0200

    implemented bootstrap

--- data.js
+++ data.js
@@ -1,15 +1,33 @@
 $(document).ready(function(){
-    var api_url = "https://reqres.in/api/users?page=2";
+    var userTable = $("#users");
 
     $.ajax({
-        url: api_url,
+        url: "https://reqres.in/api/users?page=2",
         contentType: "application/json",
         datatype: "json"
     }).done(function(data){
         console.log(data);
-        $.each(data.data, function(index, obj){
-            console.log(obj);
-            $("#list").append("<li>" + obj.first_name + " " + obj.last_name +"</li>");
+        $.each(data.data, function(index, user){
+            console.log(user);
+            var userObject = createTableRow(user.avatar, user.first_name, user.last_name, user.email);
+            userObject.hide().appendTo(userTable).show('normal');
+        });
+    });
+
+    $("#btnAddUser").on("click", function(){
+        var rnd = Math.floor(((Math.random() * 12) + 1));
+        $.ajax({
+            url: "https://reqres.in/api/users/" + rnd,
+            contentType: "application/json",
+            datatype: "json"
+        }).done(function(user){
+            console.log(user);
+            var userObject = createTableRow(user.data.avatar, user.data.first_name, user.data.last_name, user.data.email);
+            userObject.hide().appendTo(userTable).show("slow");
         });
     });
 });
+
+function createTableRow(avatar, firstName, lastName, email){
+    return $("<tr><td><img src='"+ avatar+"' alt='avatar' class='avatar'></td><td>" + firstName + "</td><td>" + lastName +"</td><td>" + email + "</td></tr>");
+}
